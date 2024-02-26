f9cad57dcd3265f534b00cfdacc40dbb1796a366
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2024-02-26 12:52:47 +0300

    апи набросан

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,26 +1,29 @@
 class Api::V1::PostsController < Api::V1::ApplicationController
   def index
-    @pins = Pin.all
+    @posts = Post.all
   end
 
   def show
-    @pin = Pin.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def create
-    user = User.find_by_jti(decrypt_payload[0]['jti'])
-    @pin = user.pins.new(pin_params)
+    jti = request.headers['Authorization']
 
-    if @pin.save
-      render json: @pin, status: :created
-    else
-      render json: @pin.errors, status: :unprocessable_entity
-    end
+    user = User.find_by_jti(decrypt_payload[0]['jti'])
+    @post = user.posts.new(post_params)
+
+      if @post.save
+        render json: @post, status: :created
+      else
+        render json: @post.errors, status: :unprocessable_entity 
+      end
+
   end
 
   private
 
-  def pin_params
-    params.require(:pin).permit(:title, :description, :pin_image, :tag_list, :category_list)
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
-    end
+
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        post "sign_out", to: "sessions#destroy"
+      end
   end
 
   namespace :admin do
