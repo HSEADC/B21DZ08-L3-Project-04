dd1b0b4e1ce1c1841d868650fa3c86f263889386
кариночка <rin.ramazanova@yandex.com>
2024-03-07 22:05:32 +0300

    menubar

--- app/assets/javascripts/menubar.js
+++ app/assets/javascripts/menubar.js
@@ -1,16 +1,16 @@
-console.log("menubar");
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function(event) {
-        console.log(event);
-        var dropdowns = document.querySelectorAll('.M_Dropdown');
-        dropdowns.forEach(function(dropdown) {
-            if (dropdown.classList.contains('NotActive')) {
-                if (event.target.classList.contains('Q_Opener')) {
-                    dropdown.classList.remove('NotActive');
-                }
-            } else {
-                dropdown.classList.add('NotActive');
-            }
-        });
+console.log('menubar');
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', function (event) {
+    console.log(event);
+    var dropdowns = document.querySelectorAll('.M_Dropdown');
+    dropdowns.forEach(function (dropdown) {
+      if (dropdown.classList.contains('NotActive')) {
+        if (event.target.classList.contains('Q_DropOpener')) {
+          dropdown.classList.remove('NotActive');
+        }
+      } else {
+        dropdown.classList.add('NotActive');
+      }
     });
+  });
 });
