f2847fe0c6d640426c243abbcbc78ad9cd690ed6
кариночка <rin.ramazanova@yandex.com>
2024-03-20 18:19:00 +0300

    делаю настройки, ща могу все сломать

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,36 +1,53 @@
 class ProfilesController < ApplicationController
-    # load_and_authorize_resource
-    before_action :set_profile, only: %i[ show edit update destroy ]
-  
-    def show
-      @user = @profile.user
-      @posts = @user.posts
-    end
-  
-    def edit
-    end
-  
-    def update
-      respond_to do |format|
-        if @profile.update(profile_params)
-          format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
-          format.json { render :show, status: :ok, location: @profile }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @profile.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # private
-    #   # Use callbacks to share common setup or constraints between actions.
-      def set_profile
-        @profile = Profile.find(params[:id])
-      end
-  
-    #   # Only allow a list of trusted parameters through.
-      def profile_params
-        params.require(:profile).permit(:username, :about, :avatar, :login)
-      end
+  # load_and_authorize_resource
+  before_action :set_profile, only: %i[ show edit update destroy ]
+
+  def show
+    @user = @profile.user
+    @posts = @user.posts
   end
-  
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
+        format.json { render :show, status: :ok, location: @profile }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    @profile = @user.build_profile(profile_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to profile_url(@profile), notice: "Profile was successfully created." }
+        format.json { render :show, status: :created, location: @profile }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :about, :avatar, :login)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,13 @@
 class Profile < ApplicationRecord
     belongs_to :user
     mount_uploader :avatar, AvatarUploader
+    validates :username, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z]+\z/, message: "Используйте латиницу" }
+    validate :username_unique
+    private
+
+    def username_unique
+      if Profile.exists?(username: username) && Profile.find_by(username: username).id != self.id
+        errors.add(:username, "Занят")
+      end
+  end
 end
