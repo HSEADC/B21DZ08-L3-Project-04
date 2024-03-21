faecd04505808de89150531121f8ff522cf9a35b
кариночка <rin.ramazanova@yandex.com>
2024-03-21 13:13:30 +0300

    тут все сломалось, поэтому я заплакала и сейчас вернусь к прошлому коммиту

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,53 +1,54 @@
 class ProfilesController < ApplicationController
-  # load_and_authorize_resource
-  before_action :set_profile, only: %i[ show edit update destroy ]
+ # load_and_authorize_resource
+ before_action :set_profile, only: %i[ show edit update destroy ]
 
-  def show
-    @user = @profile.user
-    @posts = @user.posts
-  end
+ def show
+   @user = @profile.user
+   @posts = @user.posts
+ end
 
-  def edit
-  end
+ def edit
+ end
 
-  def update
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
-        format.json { render :show, status: :ok, location: @profile }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+ def update
+   respond_to do |format|
+     if @profile.update(profile_params)
+       format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
+       format.json { render :show, status: :ok, location: @profile }
+     else
+       format.html { render :edit, status: :unprocessable_entity }
+       format.json { render json: @profile.errors, status: :unprocessable_entity }
+     end
+   end
+end
+end
+
+def create
+  @user = User.new(user_params)
+  @profile = @user.build_profile(profile_params)
+
+  respond_to do |format|
+    if @user.save
+      format.html { redirect_to profile_url(@profile), notice: "Profile was successfully created." }
+      format.json { render :show, status: :created, location: @profile }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  def create
-    @user = User.new(user_params)
-    @profile = @user.build_profile(profile_params)
+private
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to profile_url(@profile), notice: "Profile was successfully created." }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+def set_profile
+  @profile = Profile.find(params[:id])
+end
 
-  private
+def profile_params
+  params.require(:profile).permit(:username, :about, :avatar, :login)
+end
 
-  def set_profile
-    @profile = Profile.find(params[:id])
-  end
-
-  def profile_params
-    params.require(:profile).permit(:username, :about, :avatar, :login)
-  end
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
+end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,13 @@
 class Profile < ApplicationRecord
     belongs_to :user
     mount_uploader :avatar, AvatarUploader
-    validates :username, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z]+\z/, message: "Используйте латиницу" }
-    validate :username_unique
+    validates :login, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z]+\z/, message: "Используйте латиницу" }
+    validate :login_unique
     private
 
-    def username_unique
-      if Profile.exists?(username: username) && Profile.find_by(username: username).id != self.id
-        errors.add(:username, "Занят")
+    def login_unique
+      if Profile.exists?(login: login) && Profile.find_by(login: login).id != self.id
+        errors.add(:login, "Занят")
       end
   end
 end
