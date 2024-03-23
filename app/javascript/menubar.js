8cf12c02f422880bdac11450385bfc5cb2ce1eb5
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2024-03-23 16:15:48 +0300

    menubar minor change, edit/delete buttons for post added, share button added

--- app/javascript/menubar.js
+++ app/javascript/menubar.js
@@ -1,7 +1,5 @@
-console.log('menubar');
 document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('click', function (event) {
-    console.log(event);
     var dropdowns = document.querySelectorAll('.M_Dropdown');
     dropdowns.forEach(function (dropdown) {
       if (dropdown.classList.contains('NotActive')) {
