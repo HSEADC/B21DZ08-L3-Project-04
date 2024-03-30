f5ece45082a40727729c493cc71046cfe72e6779
кариночка <rin.ramazanova@yandex.com>
2024-03-30 03:18:35 +0300

    minor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,27 +1,29 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :attachments
-    mount_uploader :post_image, PostImageUploader
-    has_rich_text :text
-    acts_as_taggable_on :tags
+  has_many :comments
+  has_many :attachments
+  mount_uploader :post_image, PostImageUploader
+  has_rich_text :text
 
-    has_many :favourites
-    has_many :users_who_favourited, through: :favourites, source: 'user'
+  acts_as_taggable_on :tags
 
-    has_many :likes
-    has_many :users_who_liked, through: :likes, source: 'user'
+  has_many :favourites
+  has_many :users_who_favourited, through: :favourites, source: 'user'
 
-    has_and_belongs_to_many :users
+  has_many :likes
+  has_many :users_who_liked, through: :likes, source: 'user'
 
-    belongs_to :user
-    def api_as_json
-        {
-          title: title,
-          description: description,
-        }
-    end
+  has_and_belongs_to_many :users
 
-    def total_comments_count
-      comments.count + comments.map { |comment| comment.replies.count }.sum
-    end
+  belongs_to :user
+
+  def api_as_json
+    {
+      title: title,
+      description: description,
+    }
+  end
+
+  def total_comments_count
+    comments.count + comments.map { |comment| comment.replies.count }.sum
+  end
 end
