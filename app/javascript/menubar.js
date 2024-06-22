51ea8d1e00dc0068fb53715ac818d3bd87eeccad
Vanurina Masha <90695332+Redvence@users.noreply.github.com>
2024-06-22 10:42:59 +0300

    collections popup fixed

--- app/javascript/menubar.js
+++ app/javascript/menubar.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('turbo:load', function () {
   const burgerButton = document.getElementById('burgerMenu');
   const dropdown = document.getElementById('mobileDropdown');
   const desktopOpener = document.getElementById('desktopOpener');
